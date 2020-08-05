6089ed81e2ed9ceea186eb4ef91a1addd6231dc3
Amita <sardana.amita2@gmail.com>
2020-08-05 19:45:22 +0530

    Add  my_each_with_index method

--- main.rb
+++ main.rb
@@ -1,8 +1,16 @@
 module Enumerable
-    def my_each
-      return enum_for unless block_given?
-      for e in self
-        yield e
-      end
+  def my_each
+    return enum_for unless block_given?
+    for e in self
+      yield e
     end
+  end
+
+  def my_each_with_index
+    return enum_for unless block_given?
+
+    for i in 0..self.length - 1
+      yield self[i], i
+    end
+  end
 end
