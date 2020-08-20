3953ad2274abcb3636564071cf42355d939eeb5b
Amita <sardana.amita2@gmail.com>
2020-08-20 17:09:41 +0530

    Add test for #my_each_with_index

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,6 +3,7 @@ require_relative '../main.rb'
 describe Enumerable do
   let(:test_array) { [20, 3, 12, 7, 15] }
   let(:test_range) { (1..9) }
+  let(:test_hash) { { 'cat' => 'meow', 'dog' => 'bark' } }
 
   describe '#my_each' do
     context 'when called with a &block' do
@@ -12,12 +13,6 @@ describe Enumerable do
 
       it 'returns a range when called on a range' do
         expect(test_range.my_each { }).to eq(test_range.each { })
-      end
-
-      context 'not equal' do
-        it 'returns true when compairing result of array and range' do
-          expect(test_array.my_each { }).not_to eq(test_range.each { })
-        end
       end
     end
 
@@ -29,15 +24,37 @@ describe Enumerable do
       it 'returns a range enumerator when called on a range' do
         expect(test_range.my_each.class).to eq(test_range.each.class)
       end
-
-      context 'not equal' do
-        it 'returns true when compairing same array result' do
-          expect(test_range.my_each).not_to eq(test_range.each)
-        end
-      end
     end
   end
 
   describe '#my_each_with_index' do
+    context 'when called with a &block' do
+      it 'returns the original array when called on an array' do
+        expect(test_array.my_each_with_index { }).to eql(test_array)
+      end
+      it 'returns the range itself when called on a range' do
+        expect(test_range.my_each_with_index { }).to eql(test_range)
+      end
+
+      it 'returns the hash itself when called on a hash' do
+        expect(test_hash.my_each_with_index { }).to eql(test_hash)
+      end
+
+      context 'returns the same result like #each_with_index' do
+        it do
+          original_each_test = test_array.each_with_index { |e, i| e + i }
+          expect(test_array.my_each_with_index { |e, i| e + i }).to eql(original_each_test)
+        end
+      end
+    end
+
+    context 'when called without a &block' do
+      it 'returns an array enumerator when called on an array' do
+        expect(test_array.my_each).to be_instance_of(Enumerator)
+      end
+      it 'returns a range enumerator when called on a range' do
+        expect(test_range.my_each).to be_instance_of(Enumerator)
+      end
+    end
   end
 end
